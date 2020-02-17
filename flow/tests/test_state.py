c711a520d763f1a80b3d6057742eea24d8c46026
Chris Richards <christopher.james.richards@gmail.com>
2020-02-17 12:54:28 -0500

    More DotDictState tests because I am paranoid

--- flow/tests/test_state.py
+++ flow/tests/test_state.py
@@ -1,9 +1,36 @@
 from flow.core.state import DotDictState
 
 
-def test_state():
-    state = DotDictState({'test': {'dict': 'val'}})
-    assert state.test.dict == 'val'
+class TestDotDictState:
+    def test_state_dot(self):
+        state = DotDictState({'test': {'dict': 'val'}})
+        assert state.test.dict == 'val'
 
-    d = state.to_dict()
-    assert d == {'test': {'dict': 'val'}}
+    def test_state_to_dict(self):
+        state = DotDictState({'test': {'dict': 'val'}})
+        d = state.to_dict()
+        assert d == {'test': {'dict': 'val'}}
+
+    def test_state_update(self):
+        state = DotDictState({'test': 'dumb'})
+        state.update({'oops': 'my bad'})
+
+        assert state.to_dict() == {'oops': 'my bad', 'test': 'dumb'}
+
+    def test_state_pop(self):
+        state = DotDictState({'test': 'dumb'})
+        val = state.pop('test')
+
+        assert val == 'dumb'
+        assert state.to_dict() == {}
+
+    def test_state_get(self):
+        state = DotDictState({'test': 'dumb'})
+        val = state.get('test')
+
+        assert val == 'dumb'
+        assert state.to_dict() == {'test': 'dumb'}
+
+    def test_nested_dot_get(self):
+        state = DotDictState({'test': {'dict': 'val'}})
+        assert state.test.get('dict') == 'val'
